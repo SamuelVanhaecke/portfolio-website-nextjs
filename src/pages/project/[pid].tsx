613cb7ccafc575317fd3fc43971b11c018899d3d
SamuelVanhaecke <samuel.vanhaecke@student.howest.be>
2023-03-29 23:23:28 +0200

    feat: added some layout + styling of projectdetail

--- src/pages/project/[pid].tsx
+++ src/pages/project/[pid].tsx
@@ -2,6 +2,7 @@
 import useSWR from 'swr'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
+import Image from 'next/image'
 
 import Layout from '@/components/Layout'
 
@@ -27,13 +28,30 @@ export default () => {
   return (
     <Layout>
       {project ? (
-        <div>
-          <h1>Project {pid}</h1>
-          <ul>
-            {/* console log data */}
-            <li>Name: {project.title}</li>
-            {/* <li>Language: {}</li> */}
-          </ul>
+        <div className="m-auto max-w-screen-2xl">
+          <h1 className="mb-9 font-ilyas text-8xl uppercase">
+            {project.title}
+          </h1>
+          <p className="mb-7 w-1/2">{project.description}</p>
+          <div>
+            {project.tags.map((tag: string) => (
+              <span
+                className="mr-3 rounded-full border border-black px-4 py-[5px] font-light"
+                key={tag}
+              >
+                {tag}
+              </span>
+            ))}
+          </div>
+          <div className="my-24 border border-black p-10">
+            <Image
+              className="w-full"
+              src={project.image}
+              alt={project.alt}
+              width={project.width}
+              height={project.height}
+            />
+          </div>
         </div>
       ) : (
         <div>Project not found</div>
