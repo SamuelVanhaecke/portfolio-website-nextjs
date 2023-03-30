73135a9c1418149ab7970cf61995fb9ec7cff7b2
SamuelVanhaecke <samuel.vanhaecke@student.howest.be>
2023-03-30 17:52:59 +0200

    feat: created components for animated background gradients

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -4,6 +4,12 @@ import useSWR from 'swr'
 // components
 import Layout from '@/components/Layout'
 import Link from 'next/link'
+import BackgroundGradient from '@/components/BackgroundGradientBlue'
+import BackgroundGradientOrange from '@/components/BackgroundGradientOrange'
+
+// images
+import blueGradient from '../../public/images/blue_big.png'
+import orangeGradient from '../../public/images/orange_big.png'
 
 const fetcher = (url: RequestInfo | URL) => fetch(url).then(res => res.json())
 
@@ -15,46 +21,55 @@ export default () => {
   if (!data) return <div>Loading...</div>
 
   const projects = JSON.parse(data)
-
   const classNames = ['self-start', 'self-center', 'self-end']
 
   return (
-    <Layout>
-      <div className="m-auto max-w-screen-xl">
-        <h1 className="font-ilyas text-8xl uppercase">
-          Take a look <br />
-          at my work
-        </h1>
-        <div className="flex flex-col">
-          {projects.map(item => (
-            <Link
-              className={`relative my-32 w-fit border border-solid border-black p-3 transition-all hover:-rotate-1 ${
-                classNames[Math.floor(Math.random() * classNames.length)]
-              }`}
-              href={`/project/${item.title}`}
-              key={item.id}
-            >
-              <Image
-                className="max-h-[60vh] min-h-[40vh] w-auto"
-                src={item.image}
-                alt={item.alt}
-                width={item.width}
-                height={item.height}
-              />
-              <ul className="absolute bottom-5 right-5 flex gap-3">
-                {item.tags.map(tag => (
-                  <li
-                    className="rounded-full border border-white px-4 py-[5px] text-sm font-light text-white"
-                    key={tag}
-                  >
-                    {tag}
-                  </li>
-                ))}
-              </ul>
-            </Link>
-          ))}
+    <>
+      <BackgroundGradient />
+      {/* <BackgroundGradientOrange /> */}
+      <Layout>
+        <div className="m-auto max-w-screen-xl">
+          {/* <Image
+            className="absolute -left-64 top-[50%] -z-50"
+            src={orangeGradient}
+            alt=""
+          /> */}
+          {/* <Image className="absolute left-56 -z-40" src={blueGradient} alt="" /> */}
+          <h1 className="font-ilyas text-8xl uppercase">
+            Take a look <br />
+            at my work
+          </h1>
+          <div className="flex flex-col">
+            {projects.map(item => (
+              <Link
+                className={`relative my-32 w-fit border border-solid border-black p-3 transition-all hover:-rotate-1 ${
+                  classNames[Math.floor(Math.random() * classNames.length)]
+                }`}
+                href={`/project/${item.title}`}
+                key={item.id}
+              >
+                <Image
+                  className="max-h-[80vh] min-h-[60vh] w-auto"
+                  src={item.image}
+                  alt={item.alt}
+                  width={item.width}
+                  height={item.height}
+                />
+                <ul className="absolute bottom-5 right-5 flex gap-3">
+                  {item.tags.map(tag => (
+                    <li
+                      className="rounded-full border border-white py-[5px] px-4 text-sm font-light text-white"
+                      key={tag}
+                    >
+                      {tag}
+                    </li>
+                  ))}
+                </ul>
+              </Link>
+            ))}
+          </div>
         </div>
-      </div>
-    </Layout>
+      </Layout>
+    </>
   )
 }
