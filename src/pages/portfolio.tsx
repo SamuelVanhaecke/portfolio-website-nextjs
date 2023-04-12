0f3462e741d869b7c38c3f9b72f404ba22b9d805
SamuelVanhaecke <samuel.vanhaecke@student.howest.be>
2023-04-12 22:48:28 +0200

    feat: portfolio is responsive

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -21,46 +21,46 @@ export default () => {
   if (!data) return <div>Loading...</div>
 
   const projects = JSON.parse(data)
-  const classNames = ['self-start', 'self-center', 'self-end']
+  const classNames = ['2xl:self-start', '2xl:self-center', '2xl:self-end']
 
   return (
     <>
       <BackgroundGradient />
       {/* <BackgroundGradientOrange /> */}
       <Layout>
-        <div className="m-auto max-w-screen-xl">
+        <div className="m-auto max-w-screen-lg px-8 md:px-24 lg:px-0">
           {/* <Image
             className="absolute -left-64 top-[50%] -z-50"
             src={orangeGradient}
             alt=""
           /> */}
           {/* <Image className="absolute left-56 -z-40" src={blueGradient} alt="" /> */}
-          <h1 className="font-ilyas text-8xl uppercase">
+          <h1 className="mb-12 font-ilyas text-6xl uppercase md:mb-20 md:text-7xl lg:text-8xl">
             Take a look <br />
             at my work
           </h1>
-          <div className="flex flex-col">
+          <div className="flex flex-col gap-24 md:gap-48 lg:gap-64">
             {/* @ts-ignore */}
             {projects.map(item => (
               <Link
-                className={`relative my-32 w-fit border border-solid border-black p-3 transition-all hover:-rotate-1 ${
+                className={`relative w-fit self-center border border-solid border-black p-3 transition-all hover:-rotate-1 ${
                   classNames[Math.floor(Math.random() * classNames.length)]
                 }`}
                 href={`/project/${item.title}`}
                 key={item.id}
               >
                 <Image
-                  className="max-h-[80vh] min-h-[60vh] w-auto"
+                  className="w-auto md:max-h-[50vh] md:min-h-[40vh] 2xl:max-h-[80vh] 2xl:min-h-[60vh]"
                   src={item.image}
                   alt={item.alt}
                   width={item.width}
                   height={item.height}
                 />
-                <ul className="absolute bottom-5 right-5 flex gap-3">
+                <ul className="absolute bottom-5 right-5 flex flex-row-reverse flex-wrap-reverse gap-3 md:flex-nowrap">
                   {/* @ts-ignore */}
                   {item.tags.map(tag => (
                     <li
-                      className="rounded-full border border-white py-[5px] px-4 text-sm font-light text-white"
+                      className="rounded-full border border-white py-1 px-4 text-xs font-light text-white md:py-[5px] md:text-sm"
                       key={tag}
                     >
                       {tag}
