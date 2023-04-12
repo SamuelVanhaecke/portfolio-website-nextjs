d2dda492152b4c678fae39e1a820fe58d253d533
SamuelVanhaecke <samuel.vanhaecke@student.howest.be>
2023-04-12 21:15:50 +0200

    feat: made navbar responsive

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -9,12 +9,54 @@ import { useState } from 'react'
 export default () => {
   const router = useRouter()
 
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
-    <nav className="relative m-auto mt-10 mb-24 flex max-w-screen-2xl justify-between">
-      <Link href="/">
-        <h1 className="font-ilyas text-5xl uppercase text-black">Samuel</h1>
-      </Link>
-      <ul className="flex items-center gap-24">
+    <nav className="relative m-auto mt-10 mb-16 flex flex-col justify-between px-8 md:mb-24 md:flex-row md:px-16 xl:px-28 ">
+      <div className="flex flex-row items-center justify-between">
+        <Link href="/">
+          <h1 className="font-ilyas text-5xl uppercase text-black">Samuel</h1>
+        </Link>
+        {/* TODO: change and animate svg's */}
+        <div className="md:hidden">
+          <button
+            className="flex h-8 w-8 flex-col justify-between"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            {isOpen ? (
+              <svg viewBox="0 0 24 24">
+                <path d="M2 6h20v2H2V6zm0 5h20v2H2v-2zm0 5h20v2H2v-2z" />
+              </svg>
+            ) : (
+              <svg viewBox="0 0 24 24">
+                <line
+                  x1="4"
+                  y1="12"
+                  x2="20"
+                  y2="12"
+                  stroke="black"
+                  stroke-width="2"
+                />
+                <line
+                  x1="12"
+                  y1="4"
+                  x2="12"
+                  y2="20"
+                  stroke="black"
+                  stroke-width="2"
+                />
+              </svg>
+            )}
+          </button>
+        </div>
+      </div>
+
+      {/* <ul className="hidden items-center gap-16 md:flex md:gap-24"> */}
+      <ul
+        className={`${
+          isOpen ? 'flex' : 'hidden'
+        } mt-8 w-full flex-col items-center justify-center gap-10 md:static md:mt-0 md:flex md:h-auto md:w-auto md:flex-row md:gap-24 md:bg-transparent`}
+      >
         <li>
           <Link
             href="/"
@@ -39,11 +81,12 @@ export default () => {
             Portfolio
           </Link>
         </li>
-        <li className="flex flex-col items-center">
+        <li className="hidden flex-col items-center md:flex">
           <button className="peer flex items-center gap-1 font-light">
-            Socials<Image src={dropdownArrow} alt="arrow pointing down"></Image>
+            Socials
+            <Image src={dropdownArrow} alt="arrow pointing down"></Image>
           </button>
-          <div className="absolute hidden flex-col items-center pt-10 transition opacity-100  duration-200 ease-out hover:flex peer-hover:flex peer-hover:translate-y-3 peer-hover:opacity-0">
+          <div className="absolute hidden flex-col items-center pt-10 opacity-100 transition  duration-200 ease-out hover:flex peer-hover:flex peer-hover:translate-y-3 peer-hover:opacity-0">
             <a
               className="px-5 py-1 font-light hover:font-medium"
               href="https://github.com/SamuelVanhaecke"
