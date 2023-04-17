3e3e93baea5bf69a65d1fb453043c12b1954cf4b
SamuelVanhaecke <samuel.vanhaecke@student.howest.be>
2023-04-17 21:36:37 +0200

    feat: added skills component

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -8,7 +8,7 @@ interface Skills {
     name: string
     skills: {
       name: string
-      level: string
+      startDate: string
     }[]
   }[]
 }
@@ -25,6 +25,20 @@ export default () => {
     }
   }, [data])
 
+  const calculateExperience = (startDate: string) => {
+    const years =
+      (Date.now() - new Date(startDate).getTime()) /
+      (1000 * 60 * 60 * 24 * 365.25)
+
+    // if years is not a round number return the number of years rounded down
+    if (years < 1) {
+      return '1-'
+    } else if (years % 1 !== 0) {
+      return Math.floor(years) + '+ years'
+    }
+    // return Math.floor(years)
+  }
+
   return (
     <div className="my-28 flex flex-wrap justify-center gap-16 md:my-64">
       {skills?.categories.map(category => {
@@ -36,10 +50,21 @@ export default () => {
                 return (
                   <div
                     key={skill.name}
-                    className="rounded-full border border-black px-5 py-2"
+                    className="relative cursor-default rounded-full border border-black"
                   >
-                    <h3 className="text-xl font-light">{skill.name}</h3>
-                    {/* <p>{skill.level}</p> */}
+                    <div className="hover:bg-blur-0 bg-blur-sm h-full w-full rounded-full bg-white px-5 py-2 transition-all duration-500 hover:opacity-0">
+                      <h3 className="text-xl font-light">{skill.name}</h3>
+                    </div>
+                    <div className="absolute left-0 top-0 -z-10 flex h-full w-full items-center justify-center overflow-hidden break-all rounded-full ">
+                      <h3 className="animate-marquee text-clip whitespace-nowrap px-2 text-xl font-light">
+                        {calculateExperience(skill.startDate)}
+                      </h3>
+                    </div>
+                    <div className="absolute left-0 top-0 -z-10 flex h-full w-full items-center justify-center overflow-hidden break-all rounded-full ">
+                      <h3 className="animate-marquee2 translate-x-full text-clip whitespace-nowrap px-2 text-xl font-light">
+                        {calculateExperience(skill.startDate)}
+                      </h3>
+                    </div>
                   </div>
                 )
               })}
